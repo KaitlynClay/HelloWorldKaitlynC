21ba57a712178386c973612341af3b839b3fb13a
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-01-24 12:24:14 -0600

    Initial Commit

---
+++ HelloWorldKaitlynC/HelloWorldKaitlynCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldKaitlynCApp.swift
+//  HelloWorldKaitlynC
+//
+//  Created by student on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldKaitlynCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
